6c873189b336b1aa36bae6f6590485088beed128
Doug Emery <emeryr@upenn.edu>
2019-10-31 15:48:22 -0400

    Better documentation; add revisionDesc if missing

--- update_oxford_prov.rb
+++ update_oxford_prov.rb
@@ -1,14 +1,14 @@
 require 'nokogiri'
 
-# change to the correct path to the desired collection xml files...
-#files = Dir['/home/hellerb/Projects/medieval-mss/collections/Lyell/*.xml'].reject { |fn| fn.include? "empt" }
+# Set COLLECTIONS_DIR to the collections directory in the medieval-mss
+# repository on your local host.
+COLLECTIONS_DIR = "/Users/emeryr/code/GIT/medieval-mss/collections"
+# All the collection folders to modify, separated by white space.
+# e.g., COLLECTIONS = %w{ Rawl_A Rawl_B Rawl_C Rawl_D Rawl_Essex Rawl_G Rawl_liturg Rawl_poet Rawl_Q Rawl_statutes }
+COLLECTIONS = %w{ Barlow }
 
-# set this to the Collections directory on your local host
-TEI_DIR = "/Users/emeryr/code/GIT/medieval-mss/collections"
-# All the collections folders to modify, separated by white space
-COLLECTIONS = %w{ Arch_Selden Selden_superius Selden_supra }
-
-files = COLLECTIONS.flat_map { |coll| Dir["#{TEI_DIR}/#{coll}/*.xml"].reject { |fn| fn.include? "empt" } }
+# for each collection; get the list of TEI files
+files = COLLECTIONS.flat_map { |coll| Dir["#{COLLECTIONS_DIR}/#{coll}/*.xml"].reject { |fn| fn.include? "empt" } }
 
 files.each do |file|
   doc = Nokogiri::XML(open(file))
@@ -23,38 +23,48 @@ files.each do |file|
     puts "Adding new HISTORY tag for: #{file}"
   end
 
+  # /TEI/teiHeader[1]/revisionDesc[1]
+  if (revisionDesc = doc.css("teiHeader > revisionDesc").first)
+    # pass
+  else
+    revisionDesc = doc.create_element("revisionDesc")
+    # doc.css("msDesc > additional").before(history)
+    doc.css("teiHeader > fileDesc").after(revisionDesc)
+    puts "Adding new revisionDesc tag for: #{file}"
+  end
+
   if history.search("acquisition").count > 0
     puts "Existing acquisition tag, skipping:  #{file}"
     next
   else
     provenance              = doc.create_element('provenance')
-    provenance['notBefore'] = '1584'                                  # CHANGE
-    provenance['notAfter']  = '1654'                                  # CHANGE
+    provenance['notBefore'] = '1607'                                  # CHANGE
+    provenance['notAfter']  = '1691'                                  # CHANGE
     provenance['resp']      = '#MMM'
     persName                = doc.create_element('persName')
     persName['role']        = 'formerOwner'
-    persName['key']         = 'person_51728429'                       # CHANGE
-    persName.content        = 'John Selden, 1584-1654'                # CHANGE
+    persName['key']         = 'person_465'                            # CHANGE
+    persName.content        = 'Thomas Barlow, 1607-1691'              # CHANGE
     provenance.add_child(persName)
 
     doc.at('history').add_child(provenance)
 
     acquisition             = doc.create_element('acquisition')
-    acquisition['when']     = '1659'                                  # CHANGE
+    acquisition['when']     = '1691'                                  # CHANGE
     acquisition['resp']     = '#MMM'
-    acquisition.content     = 'Acquired by the Bodleian in 1659'      # CHANGE
+    acquisition.content     = 'Bequeathed to the Bodleian in 1691'    # CHANGE
 
     doc.at('history').add_child(acquisition)
 
     change                  = doc.create_element('change')
     change["when"]          = Time.now.strftime "%Y-%m-%d"
     change["xml:id"]        = "MMM"
-    change.inner_html       = %q(Provenance and acquisition information added using <ref target=https://github.com/upenn-libraries/oxfordupdates/blob/master/test_case_for_oxford_prov.rb>https://github.com/upenn-libraries/oxfordupdates/blob/master/test_case_for_oxford_prov.rb</ref> in collaboration with the <ref target=http://mappingmanuscriptmigrations.org/>Mapping Manuscript Migrations</ref> project.')
+    change.inner_html       = %q(Provenance and acquisition information added using <ref target=https://github.com/mapping-manuscript-migrations/oxford-tei-updates/blob/master/update_oxford_prov.rb>https://github.com/mapping-manuscript-migrations/oxford-tei-updates/blob/master/update_oxford_prov.rb</ref> in collaboration with the <ref target=http://mappingmanuscriptmigrations.org/>Mapping Manuscript Migrations</ref> project.)
     change_person           = doc.create_element('persName')
     change_person.content   = 'Toby Burrows/Mapping Manuscript Migrations' # CHANGE??
     change.prepend_child(change_person)
 
-    doc.at('revisionDesc').prepend_child(change)
+    revisionDesc.prepend_child(change)
   end
 
   fp = open(file, 'w')
